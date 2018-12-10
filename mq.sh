68fb7ec6beaff786eee675d0684bfa841600ce06
Roydom <45684422+Roydom@users.noreply.github.com>
2018-12-10 20:20:44 +0800

    Update mq.sh

--- mq.sh
+++ mq.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-a=1
-b=2
-c=$(($a+$b))
-d=$(expr $a + $b)
-echo $c o $a o $b
-echo $d
-if [ '$a' == '$b' ];then
-	echo 'a==b'
-elif [ '$c' == '$d' ];then
-	echo 'okkk'
-else
-	echo 'GG'
-fi
-echo $a $b $c $d
-if echo 'hello';then
-	echo 'okkk'
-fi
+print("南妹妹你最美")
